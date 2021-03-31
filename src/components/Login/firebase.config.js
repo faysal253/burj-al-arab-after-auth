c9eab116c173b841628c2a796a643a6066bce325
Faysal Hasan <ahammedfaysal37@gmail.com>
2021-03-31 16:46:40 +0600

    initial commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBgwSfD9OtSGdFagr5zGdp0Pw7Uik7-WGU",
+  authDomain: "burj-al-arab-724b3.firebaseapp.com",
+  projectId: "burj-al-arab-724b3",
+  storageBucket: "burj-al-arab-724b3.appspot.com",
+  messagingSenderId: "888515988623",
+  appId: "1:888515988623:web:2e4bb5b6716babc813f4fa"
+};
+export default firebaseConfig;
